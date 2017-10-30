eff06a79adf07a8d80670f63916c59039e458665
kfcaby <kfcaby@gmail.com>
2017-10-29 20:40:02 -0400

    Running only 2005 now.

--- fit_emissions_networks.R
+++ fit_emissions_networks.R
@@ -1,19 +1,21 @@
 
 rm(list=ls())
-setwd("/nfs/nsaph_ci3/users/ci3_kcummiskey/emissionsNetworks")
+
 
 library(data.table)
 library(geosphere)
 library(lubridate)
 library(tidyr)
 library(mgcv)
+library(maps)
+library(maptools)
 source("./functions_emissions_networks.R")
 
 ##-----------------------------------------------------------------------##
 ##            Create Emissions Networks                                  ##
 ##-----------------------------------------------------------------------##
 
-load(file = "../Data/daily_emissions_facility_temperature.RData")
+load(file = "daily_emissions_facility_temperature.RData")
 setkey(M_locations, ID)
 setkey(PP_locations, ID)
 
@@ -24,16 +26,27 @@ end.day <- "08-31"   #MM-DD
 
 #Other parameters
 max.distance = 1000 #max distance between edges
-percent.of.powerplants = 10 #percent of power plants to use
-k1 = 3
+percent.of.powerplants = 20 #percent of power plants to use
+k1 = 5
 wind.speed = 13 #kph
 
+edges <- fitDailyPMmodels(2005, emissions, PM, PP_locations, M_locations, 
+                          start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
+                          k1 = k1, wind.speed = wind.speed, max.distance = max.distance)
 
-edges <- lapply(years, fitDailyPMmodels, emissions, PM, PP_locations, M_locations,
-                       start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
-                       k1 = k1, wind.speed = wind.speed, 
-                       max.distance = max.distance)
-names(edges) <- years
+cutoff.perc = 0.70
+plotEmissionsNetwork(edges, emissions, 
+                     PM, PP_locations, M_locations,
+                     main = "",plot.diagnostics = TRUE,
+                     plot.percent.of.powerplants = 100)
+plotHighLowMap(edges, PP_locations, M_locations, cutoff.perc = cutoff.perc,
+               main = "")
+
+# edges <- lapply(years, fitDailyPMmodels, emissions, PM, PP_locations, M_locations,
+#                        start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
+#                        k1 = k1, wind.speed = wind.speed, 
+#                        max.distance = max.distance)
+# names(edges) <- years
 
 
 #save(list = "edges", file = "./edges2003_2016_1000max2.RData")
