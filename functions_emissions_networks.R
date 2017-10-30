9b868e2a989194aa2cac1462973837542021c24e
kfcaby <kfcaby@gmail.com>
2017-10-30 10:57:25 -0400

    Add knots to edges info.

--- functions_emissions_networks.R
+++ functions_emissions_networks.R
@@ -177,6 +177,7 @@ fitDailyPMmodels <- function(year, emissions, PM, PP_locations, M_locations ,sta
   pairs$end.day <- end.day
   pairs$year <- year
   pairs$wind.speed <- wind.speed
+  pairs$knots <- k1
   
   print(paste("The number of edges is:",sum(pairs$edge,na.rm = TRUE) ,sep = " "))
   print(paste("Edge density is:",round(sum(pairs$edge,na.rm = TRUE)/sum(!is.na(pairs$edge)),2), sep = ""))
