2ea0ad1fd7d0d5a0646f7eb49d30ee4acb92c52d
kfcaby <kfcaby@gmail.com>
2017-11-01 16:46:08 -0400

    Modified for running on cluster.

--- fit_emissions_networks.R
+++ fit_emissions_networks.R
@@ -1,6 +1,4 @@
-
 rm(list=ls())
-
 
 library(data.table)
 library(geosphere)
@@ -9,24 +7,20 @@ library(tidyr)
 library(mgcv)
 library(maps)
 library(maptools)
-source("./functions_emissions_networks.R")
-
+source("functions_emissions_networks.R")
+load(file = "PMimputed.RData")
+load(file = "../decomposePM/PMimputedlowfreq.RData")
 ##-----------------------------------------------------------------------##
 ##            Create Emissions Networks                                  ##
 ##-----------------------------------------------------------------------##
 
-load(file = "daily_emissions_facility_temperature.RData")
-setkey(M_locations, ID)
-setkey(PP_locations, ID)
-
 #time period of interest
-years <- 2005:2006
 start.day <- "06-01" #MM-DD
 end.day <- "08-31"   #MM-DD
 
 #Other parameters
 max.distance = 1000 #max distance between edges
-percent.of.powerplants = 20 #percent of power plants to use
+percent.of.powerplants = 100 #percent of power plants to use
 k1 = 5
 wind.speed = 13 #kph
 
@@ -34,19 +28,12 @@ edges <- fitDailyPMmodels(2005, emissions, PM, PP_locations, M_locations,
                           start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
                           k1 = k1, wind.speed = wind.speed, max.distance = max.distance)
 
-cutoff.perc = 0.70
-plotEmissionsNetwork(edges, emissions, 
-                     PM, PP_locations, M_locations,
-                     main = "",plot.diagnostics = TRUE,
-                     plot.percent.of.powerplants = 100)
-plotHighLowMap(edges, PP_locations, M_locations, cutoff.perc = cutoff.perc,
-               main = "")
+edges_imputed <- fitDailyPMmodels(2005, emissions, PM.imputed, PP_locations, M_locations, 
+                          start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
+                          k1 = k1, wind.speed = wind.speed, max.distance = max.distance)
 
-# edges <- lapply(years, fitDailyPMmodels, emissions, PM, PP_locations, M_locations,
-#                        start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
-#                        k1 = k1, wind.speed = wind.speed, 
-#                        max.distance = max.distance)
-# names(edges) <- years
+edges_lowfreq <- fitDailyPMmodels(2005, emissions, PM.imputed.lowfreq3, PP_locations, M_locations, 
+                          start.day, end.day, percent.of.powerplants = percent.of.powerplants, 
+                          k1 = k1, wind.speed = wind.speed, max.distance = max.distance)
 
-
-#save(list = "edges", file = "./edges2003_2016_1000max2.RData")
+save(list = c("edges","edges_imputed","edges_lowfreq"), file = "edges2005_imputed_decomposed.RData")
