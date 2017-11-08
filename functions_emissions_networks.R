147b9c42e651d51512b5f0d4d06850ce1566c2bd
kfcaby <kfcaby@gmail.com>
2017-11-07 22:09:10 -0500

    added the get Region function.

--- functions_emissions_networks.R
+++ functions_emissions_networks.R
@@ -1,5 +1,24 @@
 # Basic functions for manipulating emissions/PM time series and creating an emissions network
 
+getRegion <- function(states){
+  Northeast = c("ME", "NH", "VT", "NY", "PA", "DE", "NJ", "MD", "DC", "VA", "MA", "CT", "RI")
+  IndustrialMidwest = c("WV", "OH", "KY", "IN", "IL", "WI", "MI")
+  Southeast = c("FL", "GA", "SC", "NC", "TN", "AL", "MS", "AR","LA")
+  UpperMidwest = c("MN", "IA", "MO", "KS", "NE", "SD", "ND")
+  Southwest = c("TX", "OK", "NM", "AZ")
+  SouthernCalifornia = c("CA")
+  Northwest = c("NV", "UT", "CO", "WY", "MT", "ID", "OR", "WA")
+  regions <- ifelse(states %in% Northeast,"Northeast",
+                    ifelse(states %in% IndustrialMidwest, "IndustrialMidwest",
+                           ifelse(states %in% Southeast, "Southeast",
+                                  ifelse(states %in% UpperMidwest, "UpperMidwest",
+                                         ifelse(states %in% Southwest, "Southwest",
+                                                ifelse(states %in% SouthernCalifornia, "SouthernCalifornia",
+                                                       ifelse(states %in% Northwest,"Northwest", NA)))))))
+  return(regions)
+}
+
+
 #convert a ZIP code from 3-digit to 5-digit format
 convertZip <- function(zip){
   temp_zip<-formatC(zip, width = 5, format = "d", flag = "0")
