99b13693e41a6e97b4b8d08c570ccb9e9e729975
kfcaby <kfcaby@gmail.com>
2017-11-20 16:01:13 -0500

    excludes 0 gams.coeff from correlation calculation

--- R/rankComparison.R
+++ R/rankComparison.R
@@ -2,20 +2,34 @@
 rankComparison <- function(edges, var1, var2, regions = c("Northeast","Southeast","IndustrialMidwest")){
   
   edges <- subset(edges, receptor.region %in% regions)
+  include <- 1:length(unique(edges$Monitor)) 
   
   setkey(edges, Monitor)
   if(var1 == "gams.coeff"){
     var1.monitor <- edges[ , sum(get(var1)*edge, na.rm = TRUE), by = "Monitor"]$V1
-  } else {
+    include <- which(var1.monitor > 0)
+  } 
+  if(var1 == "avgPM") { 
+    var1.monitor <- edges[ , unique(get(var1)), by = "Monitor"]$V1
+  }
+  if(var1 == "inmapPM") { 
     var1.monitor <- edges[ , sum(get(var1), na.rm = TRUE), by = "Monitor"]$V1
   }
   
   if(var2 == "gams.coeff"){
     var2.monitor <- edges[ , sum(get(var2)*edge, na.rm = TRUE), by = "Monitor"]$V1
-  } else {
+    include <- which(var1.monitor > 0)
+  } 
+  if(var2 == "avgPM") { 
+    var2.monitor <- edges[ , unique(get(var2)), by = "Monitor"]$V1
+  }
+  if(var2 == "inmapPM") { 
     var2.monitor <- edges[ , sum(get(var2), na.rm = TRUE), by = "Monitor"]$V1
   }
-  correlation <- cor(rank(var1.monitor), rank(var2.monitor))
+  
+  
+  
+  correlation <- cor(rank(var1.monitor[include]), rank(var2.monitor[include]))
   plot(rank(var1.monitor),rank(var2.monitor), 
        xlab= paste("Ranked Exposure ",var1, sep = ""),
        ylab = paste("Ranked Exposure ",var2, sep = ""), 
