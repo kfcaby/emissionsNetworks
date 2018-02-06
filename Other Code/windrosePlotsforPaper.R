8160953bcd373c9fc113f504c5fb37f72f0680c7
kfcaby <kfcaby@gmail.com>
2018-02-06 10:05:51 -0500

    added functions to make this easier

--- Other Code/windrosePlotsforPaper.R
+++ Other Code/windrosePlotsforPaper.R
@@ -49,91 +49,103 @@ createWindroseData <- function(edges, regions, distlim = c(0,1000)){
   return(data)
 }
 
+plotEdgeCounts <- function(edges, regions){
+  data <- createWindroseData(edges, regions = regions)
+  txt.size = 12
+  legend.position = "none"
+  title = ""
+  plot <- ggplot(data = subset(data, edge == 1),aes(x = dir.binned, fill = distance.binned)) +
+    geom_bar() + 
+    scale_x_discrete(drop = FALSE,
+                     labels = c("N","","","E","","","S","","","W","","")) + 
+    coord_polar(start = -((30/2)/360) * 2*pi) +
+    scale_fill_manual(name = "Distance to Source (km)",
+                      labels = c("750-1000","500-750","250-500","0-250"),
+                      values = viridis(4),
+                      drop = FALSE) +
+    theme(axis.title.x = element_blank(),
+          panel.grid.major = element_line(colour="grey65"),
+          legend.position = legend.position,
+          axis.title = element_text(size = txt.size),
+          axis.text.x = element_text(size = txt.size),
+          axis.text.y = element_text(size = 8),
+          #axis.ticks.y = element_blank(),
+          axis.title.y = element_blank(),
+          legend.text = element_text(size = txt.size),
+          legend.title = element_text(size = txt.size),
+          plot.title = element_text(size = 12, hjust = 0.5)) + 
+    #  ylim(ylim[1],ylim[2]) +
+    ggtitle(title)
+  return(plot)
+}
+
+plotEdgeProbs <- function(edges, regions){
+  data <- createWindroseData(edges, regions = regions)
+  data_summary <- data[ , list(prob = sum(prob)),
+                        by = c("distance.binned","dir.binned")]
+  txt.size = 12
+  title = ""
+  legend.position = "none"
+  plot <- ggplot(data = data_summary, aes(x = dir.binned, y = prob, fill = distance.binned)) +
+    geom_col() + scale_x_discrete(drop = FALSE,
+                                  labels = c("N","","","E","","","S","","","W","","")) +
+    coord_polar(start = -((30/2)/360) * 2*pi) +
+    scale_fill_manual(name = "Distance to Source (km)",
+                      labels = c("750-1000","500-750","250-500","0-250"),
+                      values = viridis(4),
+                      drop = FALSE) +
+    theme(axis.title.x = element_blank(),
+          panel.grid.major = element_line(colour="grey65"),
+          legend.position = legend.position,
+          axis.title = element_blank(),
+          axis.text.x = element_text(size = txt.size),
+          axis.text.y = element_text(size = 8),
+          #axis.ticks.y = element_blank(),
+          legend.text = element_text(size = txt.size),
+          legend.title = element_text(size = txt.size),
+          plot.title = element_text(size = 12, hjust = 0.5)) + 
+  #  ylim(ylim[1],ylim[2]) + 
+    ggtitle(title)
+  
+  return(plot)
+}
+
 
 #Edge Counts Plots
-#plot 1
 pdf(file = "results/windrose_edgecount.pdf", width = 6.5, height = 2)
-txt.size = 12
-legend.position = "none"
-title = ""
-ylim = c(0,3000)
-data <- createWindroseData(edges, regions = "IndustrialMidwest")
-p1 <- ggplot(data = subset(data, edge == 1),aes(x = dir.binned, fill = distance.binned)) +
-  geom_bar() + 
-  scale_x_discrete(drop = FALSE,
-                   labels = c("N","","","E","","","S","","","W","","")) + 
-  coord_polar(start = -((30/2)/360) * 2*pi) +
-  scale_fill_manual(name = "Distance to Source (km)",
-                    labels = c("750-1000","500-750","250-500","0-250"),
-                    values = viridis(4),
-                    drop = FALSE) +
-  theme(axis.title.x = element_blank(),
-        panel.grid.major = element_line(colour="grey65"),
-        legend.position = legend.position,
-        axis.title = element_text(size = txt.size),
-        axis.text.x = element_text(size = txt.size),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title.y = element_blank(),
-        legend.text = element_text(size = txt.size),
-        legend.title = element_text(size = txt.size),
-        plot.title = element_text(size = 12, hjust = 0.5)) + 
-  ylim(ylim[1],ylim[2]) +
-  ggtitle(title)
-#plot 2
-title = ""
-data <- createWindroseData(edges, regions = "Northeast")
-p2 <- ggplot(data = subset(data, edge == 1),aes(x = dir.binned, fill = distance.binned)) +
-  geom_bar() + 
-  scale_x_discrete(drop = FALSE,
-                   labels = c("N","","","E","","","S","","","W","","")) + 
-  coord_polar(start = -((30/2)/360) * 2*pi) +
-  scale_fill_manual(name = "Distance to Source (km)",
-                    labels = c("750-1000","500-750","250-500","0-250"),
-                    values = viridis(4),
-                    drop = FALSE) +
-  theme(axis.title.x = element_blank(),
-        panel.grid.major = element_line(colour="grey65"),
-        legend.position = legend.position,
-        axis.title = element_blank(),
-        axis.text.x = element_text(size = txt.size),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.text = element_text(size = txt.size),
-        legend.title = element_text(size = txt.size),
-        plot.title = element_text(size = 12, hjust = 0.5)) + 
-  ylim(ylim[1],ylim[2]) +
-  ggtitle(title)
-#plot 3
-title = ""
-data <- createWindroseData(edges, regions = "Southeast")
-p3 <- ggplot(data = subset(data, edge == 1),aes(x = dir.binned, fill = distance.binned)) +
-  geom_bar() + 
-  scale_x_discrete(drop = FALSE,
-                   labels = c("N","","","E","","","S","","","W","","")) + 
-  coord_polar(start = -((30/2)/360) * 2*pi) +
-  scale_fill_manual(name = "Distance to Source (km)",
-                    labels = c("750-1000","500-750","250-500","0-250"),
-                    values = viridis(4),
-                    drop = FALSE) +
-  theme(axis.title.x = element_blank(),
-        panel.grid.major = element_line(colour="grey65"),
-        legend.position = legend.position,
-        axis.title = element_blank(),
-        axis.text.x = element_text(size = txt.size),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.text = element_text(size = txt.size),
-        legend.title = element_text(size = txt.size),
-        plot.title = element_text(size = 12, hjust = 0.5)) + 
-  ylim(ylim[1],ylim[2]) +
-  ggtitle(title)
+p1 <- plotEdgeCounts(edges, regions = "IndustrialMidwest")
+p2 <- plotEdgeCounts(edges, regions = "Northeast")
+p3 <- plotEdgeCounts(edges, regions = "Southeast")
 grid.arrange(p1,p2,p3, ncol = 3)
 dev.off()
 
 #Finish implementing this code for the other plots
 
 #Edge Probability Plots
+pdf(file = "results/windrose_edgeprob.pdf", width = 6.5, height = 2)
+p1 <- plotEdgeProbs(edges, regions = "IndustrialMidwest")
+p2 <- plotEdgeProbs(edges, regions = "Northeast")
+p3 <- plotEdgeProbs(edges, regions = "Southeast")
+grid.arrange(p1,p2,p3, ncol = 3)
+dev.off()
+
+#Edge Probability Plots (<250km)
+pdf(file = "results/windrose_edgeprob250.pdf", width = 6.5, height = 2)
+p1 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "IndustrialMidwest")
+p2 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "Northeast")
+p3 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "Southeast")
+grid.arrange(p1,p2,p3, ncol = 3)
+dev.off()
+
+pdf(file = "results/windrose_edgeprob250.pdf", width = 6.5, height = 2)
+p1 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "IndustrialMidwest")
+p2 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "Northeast")
+p3 <- plotEdgeProbs(edges[distance_cat == 1, ], regions = "Southeast")
+grid.arrange(p1,p2,p3, ncol = 3)
+dev.off()
+
+
+
 #plot 1
 pdf(file = "results/windrose_edgeprob.pdf", width = 6.5, height = 2)
 txt.size = 12
